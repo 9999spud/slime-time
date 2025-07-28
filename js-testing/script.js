a032572e0bf4a42c02027d339ce4934a28c5773f
9999spud <9999spud@gmail.com>
2025-07-28 11:16:48 -0700

    Creating a working folder for main project files

--- js-testing/script.js
+++ js-testing/script.js
@@ -10,3 +10,27 @@ let name = "John";
 let admin = "";
 
 admin = name;
+
+//prompts and if statements
+
+function promptLogin() {
+    let username = prompt('Enter username', '');
+    let password = '';
+
+
+    if (username === null || username === '') {
+        alert('cancelled');
+    } else if (username == 'Admin') {
+
+        let password = prompt('Enter password', '');
+
+        if (password == 'TheBoss') {
+            alert('Welcome');
+        } else {
+            alert('Wrong password');
+        }
+    } else {
+        console.log(username);
+        alert('I don\'t know you');
+    }
+}
