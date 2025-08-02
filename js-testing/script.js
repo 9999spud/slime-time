376812e373b1eb5bbaee85b0037142c3262ab933
9999spud <9999spud@gmail.com>
2025-08-01 20:04:03 -0700

    added an attempt at data structure for dialogue tree

--- js-testing/script.js
+++ js-testing/script.js
@@ -9,9 +9,55 @@ function showAlert(message) {
 let name = "John";
 let admin = "";
 
+const jsonString = `{
+    "forest": [
+        {
+            "meetBird": [
+                "meet",
+                "meet2",
+                "meet3"
+            ],
+            "askBirdOut": [
+                "ask", "ask2", "ask3"
+            ]
+        }
+    ],
+    "river": [
+        {
+            
+        
+        }
+    ]
+}`
+
+const data = JSON.parse(jsonString);
+const meetBirdArray = data.forest[0].meetBird;
+console.log(meetBirdArray[1]);
 admin = name;
 
 function showPrompt(message, defaultInputText) {
     let userInput = prompt(message, defaultInputText);
     alert(`You are ${userInput} years old!`);
+}
+
+let primeQuestion = 1;
+
+function isNumberPrime() {
+    primeQuestion = document.getElementById("primeq").value;
+    if (primeQuestion) {
+        for (let i = 0; i <= primeQuestion; i++) {
+            if (primeQuestion % i == 0) {
+                console.log(i);
+            }
+        }
+    } else {
+        alert("please enter a valid number");
+    }
+}
+
+function pow(a, b) {
+    for (let i = 0; i <= b; i++) {
+        a *= a;
+    }
+    console.log(a);
 }
